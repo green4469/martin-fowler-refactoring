11a1b365d542b3c56d4fbf2bc5f85f0594efe99c
yumin <green4469@gmail.com>
2020-06-30 03:54:30 +0900

    Extract volume credit logic as a method

--- ch1/main.js
+++ ch1/main.js
@@ -25,6 +25,14 @@ function statement(invoice, plays) {
         return plays[perf.playID];
     }
 
+    function volumeCreditsFor(perf) {
+        let volumeCredits = 0;
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        if ("comedy" === playFor(perf).type)
+            volumeCredits += Math.floor(perf.audience / 5);
+        return volumeCredits;
+    }
+
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
@@ -35,12 +43,7 @@ function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        // 포인트를 적립한다.
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
-
-        // 청구 내역을 출력한다.
+        volumeCredits += volumeCreditsFor(perf);
         result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
