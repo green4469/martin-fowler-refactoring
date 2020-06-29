78ff0792affd4a9b0ba18266b0b6b46a250a54ba
yumin <green4469@gmail.com>
2020-06-30 03:33:48 +0900

    Change var name into more specific one

--- ch1/main.js
+++ ch1/main.js
@@ -1,24 +1,24 @@
 function statement(invoice, plays) {
     function amountFor(aPerformance, play) {
-        let thisAmount;
+        let result;
         switch (play.type) {
             case "tragedy":
-                thisAmount = 40000;
+                result = 40000;
                 if (aPerformance.audience > 30) {
-                    thisAmount += 1000 * (aPerformance.audience - 30);
+                    result += 1000 * (aPerformance.audience - 30);
                 }
                 break;
             case "comedy":
-                thisAmount = 30000;
+                result = 30000;
                 if (aPerformance.audience > 20) {
-                    thisAmount += 10000 + 500 * (aPerformance.audience - 20);
+                    result += 10000 + 500 * (aPerformance.audience - 20);
                 }
-                thisAmount += 300 * aPerformance.audience;
+                result += 300 * aPerformance.audience;
                 break;
             default:
                 throw new Error('알 수 없는 장르: ${play.type}');
         }
-        return thisAmount;
+        return result;
     }
 
     let totalAmount = 0;
