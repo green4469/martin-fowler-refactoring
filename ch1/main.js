069fec2bbee24f24989dce3581140b3e726178c4
yumin <green4469@gmail.com>
2020-06-30 03:47:38 +0900

    Inline local variable

--- ch1/main.js
+++ ch1/main.js
@@ -21,6 +21,10 @@ function statement(invoice, plays) {
         return result;
     }
 
+    function playFor(perf) {
+        return plays[perf.playID];
+    }
+
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
@@ -30,21 +34,16 @@ function statement(invoice, plays) {
         minimumFractionDigits: 2
     }).format;
 
-    function playFor(perf) {
-        return plays[perf.playID];
-    }
-
     for (let perf of invoice.performances) {
-        const play = playFor(perf);
         const thisAmount = amountFor(perf);
 
         // 포인트를 적립한다.
         volumeCredits += Math.max(perf.audience - 30, 0);
         // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
         // 청구 내역을 출력한다.
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
+        result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
         totalAmount += thisAmount;
     }
     result += `총액: ${format(totalAmount/100)}\n`;
