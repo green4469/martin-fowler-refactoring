39393396d55f367ae124ebf119f1c0fce39e6854
yumin <green4469@gmail.com>
2020-06-30 23:37:57 +0900

    Remove local variable

--- ch1/main.js
+++ ch1/main.js
@@ -1,4 +1,16 @@
 function statement(invoice, plays) {
+    let totalAmount = 0;
+    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+
+    for (let perf of invoice.performances) {
+        result += ` ${playFor(perf).name}: ${centToUsd(amountFor(perf))} (${perf.audience}석)\n`;
+        totalAmount += amountFor(perf);
+    }
+
+    result += `총액: ${centToUsd(totalAmount)}\n`;
+    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+    return result;
+
     function amountFor(aPerformance) {
         let result;
         switch (playFor(aPerformance).type) {
@@ -41,18 +53,13 @@ function statement(invoice, plays) {
         }).format(aNumber/100);
     }
 
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-
-    for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-        result += ` ${playFor(perf).name}: ${centToUsd(amountFor(perf))} (${perf.audience}석)\n`;
-        totalAmount += amountFor(perf);
+    function totalVolumeCredits() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
     }
-    result += `총액: ${centToUsd(totalAmount)}\n`;
-    result += `적립 포인트: ${volumeCredits}점\n`;
-    return result;
 }
 
 let invoices = require('./invoices.json');
