58ae01c85add351d9f85ee8c485aab2b82824def
yumin <green4469@gmail.com>
2020-06-30 23:48:53 +0900

    Extract text render function

--- ch1/main.js
+++ ch1/main.js
@@ -1,10 +1,12 @@
 function statement(invoice, plays) {
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+    return renderPlainText(invoice, plays);
+}
 
+function renderPlainText(invoice, plays) {
+    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
     for (let perf of invoice.performances) {
         result += ` ${playFor(perf).name}: ${centToUsd(amountFor(perf))} (${perf.audience}석)\n`;
     }
-
     result += `총액: ${centToUsd(totalAmount())}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
