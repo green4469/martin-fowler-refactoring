7e67e04611aa568cd3d475be08a39b5e8e985d8f
yumin <green4469@gmail.com>
2020-06-30 21:58:50 +0900

    Rename into more readable function name

--- ch1/main.js
+++ ch1/main.js
@@ -33,12 +33,12 @@ function statement(invoice, plays) {
         return result;
     }
 
-    function format(aNumber) {
+    function centToUsd(aNumber) {
         return new Intl.NumberFormat("en-US", {
             style: "currency",
             currency: "USD",
             minimumFractionDigits: 2
-        }).format(aNumber);
+        }).format(aNumber/100);
     }
 
     let totalAmount = 0;
@@ -47,10 +47,10 @@ function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
-        result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
+        result += ` ${playFor(perf).name}: ${centToUsd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `총액: ${format(totalAmount/100)}\n`;
+    result += `총액: ${centToUsd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
 }
