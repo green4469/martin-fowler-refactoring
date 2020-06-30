3c74fc557c7c4d9e9ccb50e090d0ef18ebb64838
yumin <green4469@gmail.com>
2020-06-30 23:40:18 +0900

    Remove local variable

--- ch1/main.js
+++ ch1/main.js
@@ -1,13 +1,11 @@
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
     for (let perf of invoice.performances) {
         result += ` ${playFor(perf).name}: ${centToUsd(amountFor(perf))} (${perf.audience}석)\n`;
-        totalAmount += amountFor(perf);
     }
 
-    result += `총액: ${centToUsd(totalAmount)}\n`;
+    result += `총액: ${centToUsd(totalAmount())}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
 
@@ -59,6 +57,14 @@ function statement(invoice, plays) {
             result += volumeCreditsFor(perf);
         }
         return result;
+    }
+
+    function totalAmount() {
+        let totalAmount = 0;
+        for (let perf of invoice.performances) {
+            totalAmount += amountFor(perf);
+        }
+        return totalAmount;
     }
 }
 
