45f51165a1f80d70902df8fcd96587326f2695c5
yumin <green4469@gmail.com>
2020-07-02 07:26:26 +0900

    Use volume credits in data

--- ch1/main.js
+++ ch1/main.js
@@ -6,8 +6,9 @@ function statement(invoice) {
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result)
-        result.amount = amountFor(result)
+        result.play = playFor(result);
+        result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
@@ -36,6 +37,14 @@ function statement(invoice) {
         }
         return result;
     }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === aPerformance.play.type)
+            result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
 }
 
 
@@ -48,14 +57,6 @@ function renderPlainText(data) {
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
 
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type)
-            result += Math.floor(aPerformance.audience / 5);
-        return result;
-    }
-
     function centToUsd(aNumber) {
         return new Intl.NumberFormat("en-US", {
             style: "currency",
@@ -67,7 +68,7 @@ function renderPlainText(data) {
     function totalVolumeCredits() {
         let result = 0;
         for (let perf of data.performances) {
-            result += volumeCreditsFor(perf);
+            result += perf.volumeCredits;
         }
         return result;
     }
