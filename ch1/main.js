004cead6c3b735cbabfe79755bce874d458d124a
yumin <green4469@gmail.com>
2020-07-02 06:12:25 +0900

    Add performances data

--- ch1/main.js
+++ ch1/main.js
@@ -1,12 +1,13 @@
 function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances;
     return renderPlainText(statementData, invoice, plays);
 }
 
 function renderPlainText(data, invoice, plays) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
         result += ` ${playFor(perf).name}: ${centToUsd(amountFor(perf))} (${perf.audience}석)\n`;
     }
     result += `총액: ${centToUsd(totalAmount())}\n`;
