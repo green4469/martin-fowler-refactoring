8f67bdc2b5371ab767855fdc5dfdde3a5db92e87
yumin <green4469@gmail.com>
2020-07-02 06:16:46 +0900

    Remove unnecessary parameter plays

--- ch1/main.js
+++ ch1/main.js
@@ -3,10 +3,10 @@ function statement(invoice, plays) {
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances;
     statementData.plays = plays;
-    return renderPlainText(statementData, plays);
+    return renderPlainText(statementData);
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
     for (let perf of data.performances) {
         result += ` ${playFor(perf).name}: ${centToUsd(amountFor(perf))} (${perf.audience}석)\n`;
