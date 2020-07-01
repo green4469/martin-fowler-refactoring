7a9233c25d1c2f3c52782c3e3d9debc1aef06228
yumin <green4469@gmail.com>
2020-07-02 06:27:00 +0900

    Use play in data, not query

--- ch1/main.js
+++ ch1/main.js
@@ -19,7 +19,7 @@ function statement(invoice, plays) {
 function renderPlainText(data) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
     for (let perf of data.performances) {
-        result += ` ${playFor(perf).name}: ${centToUsd(amountFor(perf))} (${perf.audience}석)\n`;
+        result += ` ${perf.play.name}: ${centToUsd(amountFor(perf))} (${perf.audience}석)\n`;
     }
     result += `총액: ${centToUsd(totalAmount())}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
@@ -27,7 +27,7 @@ function renderPlainText(data) {
 
     function amountFor(aPerformance) {
         let result;
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -42,19 +42,15 @@ function renderPlainText(data) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error('알 수 없는 장르: ${playFor(aPerformance).type}');
+                throw new Error('알 수 없는 장르: ${aPerformance.play.type}');
         }
         return result;
-    }
-
-    function playFor(perf) {
-        return data.plays[perf.playID];
     }
 
     function volumeCreditsFor(aPerformance) {
         let result = 0;
         result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === playFor(aPerformance).type)
+        if ("comedy" === aPerformance.play.type)
             result += Math.floor(aPerformance.audience / 5);
         return result;
     }
