a089774e9214a5dffda8339a0796f88bcc01c4fc
yumin <green4469@gmail.com>
2020-07-18 17:54:35 +0900

    Extract data process logic into separate file

--- ch1/createStatementData.js
+++ ch1/createStatementData.js
@@ -1,7 +1,3 @@
-function statement(invoice, plays) {
-    return renderPlainText(createStatementData(invoice, plays));
-}
-
 function createStatementData(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
@@ -61,39 +57,4 @@ function createStatementData(invoice, plays) {
     }
 }
 
-function renderPlainText(data) {
-    let result = `청구 내역 (고객명: ${data.customer})\n`;
-    for (let perf of data.performances) {
-        result += ` ${perf.play.name}: ${centToUsd(perf.amount)} (${perf.audience}석)\n`;
-    }
-    result += `총액: ${centToUsd(data.totalAmount)}\n`;
-    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
-    return result;
-
-    function centToUsd(aNumber) {
-        return new Intl.NumberFormat("en-US", {
-            style: "currency",
-            currency: "USD",
-            minimumFractionDigits: 2
-        }).format(aNumber/100);
-    }
-}
-
-let invoices = require('./invoices.json');
-let plays = require('./plays.json');
-
-function testStatement(invoices, plays) {
-    const result = statement(invoices, plays);
-    console.log(result);
-    const actual =
-`청구 내역 (고객명: BigCo)
- Hamlet: $650.00 (55석)
- As You Like It: $580.00 (35석)
- Othello: $500.00 (40석)
-총액: $1,730.00
-적립 포인트: 47점
-`;
-    console.log(`테스트 결과: ${actual === result ? '성공' : '실패'}`);
-}
-
-testStatement(invoices, plays);
+module.exports = createStatementData;
