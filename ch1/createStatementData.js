304bf116eba0478faec82b3bc96d8f90afdab405
yuminkim <yuminkim@woowahan.com>
2020-07-25 14:41:43 +0900

    Extract calculate amount logic into tragedy calculator

--- ch1/createStatementData.js
+++ ch1/createStatementData.js
@@ -49,11 +49,7 @@ class PerformanceCalculator {
         let result;
         switch (this.play.type) {
             case "tragedy":
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
+              throw new Error('Invalid access');
             case "comedy":
                 result = 30000;
                 if (this.performance.audience > 20) {
@@ -77,7 +73,13 @@ class PerformanceCalculator {
 }
 
 class TragedyCalculator extends PerformanceCalculator {
-
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result;
+    }
 }
 
 class ComedyCalculator extends PerformanceCalculator {
