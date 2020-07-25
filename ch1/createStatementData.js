ab1ca392a80f91101f53856b511b2096a0a720b7
yuminkim <yuminkim@woowahan.com>
2020-07-25 14:20:15 +0900

    Restructure amount logic into calculator

--- ch1/createStatementData.js
+++ ch1/createStatementData.js
@@ -3,6 +3,28 @@ class PerformanceCalculator {
         this.performance = aPerformance;
         this.play = aPlay;
     }
+
+    get amount() {
+        let result;
+        switch (this.play.type) {
+            case "tragedy":
+                result = 40000;
+                if (this.performance.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (this.performance.audience > 20) {
+                    result += 10000 + 500 * (this.performance.audience - 20);
+                }
+                result += 300 * this.performance.audience;
+                break;
+            default:
+                throw new Error('알 수 없는 장르: ${this.play.type}');
+        }
+        return result;
+    }
 }
 
 function createStatementData(invoice, plays) {
@@ -17,35 +39,13 @@ function createStatementData(invoice, plays) {
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
-        result.amount = amountFor(result);
+        result.amount = calculator.amount;
         result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
     function playFor(perf) {
         return plays[perf.playID];
-    }
-
-    function amountFor(aPerformance) {
-        let result;
-        switch (aPerformance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error('알 수 없는 장르: ${aPerformance.play.type}');
-        }
-        return result;
     }
 
     function volumeCreditsFor(aPerformance) {
