bd697b1a8b82b7647709d8bd50933d614634a376
yuminkim <yuminkim@woowahan.com>
2020-07-25 14:37:13 +0900

    Create calculators for tragedy, comedy

--- ch1/createStatementData.js
+++ ch1/createStatementData.js
@@ -1,3 +1,44 @@
+function createStatementData(invoice, plays) {
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(perf => enrichPerformance(perf));
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return statementData;
+
+    function enrichPerformance(aPerformance) {
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
+        const result = Object.assign({}, aPerformance);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
+        return result;
+    }
+
+    function createPerformanceCalculator(aPerformance, aPlay) {
+        switch (aPlay.type) {
+            case "tragedy":
+                return new TragedyCalculator(aPerformance, aPlay);
+            case "comedy":
+                return new ComedyCalculator(aPerformance, aPlay);
+            default:
+                throw new Error('알 수 없는 장르: ${aPlay.type}');
+        }
+    }
+
+    function playFor(perf) {
+        return plays[perf.playID];
+    }
+
+    function totalVolumeCredits(data) {
+        return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+    }
+
+    function totalAmount(data) {
+        return data.performances.reduce((total, p) => total + p.amount, 0);
+    }
+}
+
 class PerformanceCalculator {
     constructor(aPerformance, aPlay) {
         this.performance = aPerformance;
@@ -35,38 +76,12 @@ class PerformanceCalculator {
     }
 }
 
-function createStatementData(invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(perf => enrichPerformance(perf));
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return statementData;
+class TragedyCalculator extends PerformanceCalculator {
 
-    function enrichPerformance(aPerformance) {
-        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
-        const result = Object.assign({}, aPerformance);
-        result.play = calculator.play;
-        result.amount = calculator.amount;
-        result.volumeCredits = calculator.volumeCredits;
-        return result;
-    }
+}
 
-    function createPerformanceCalculator(aPerformance, aPlay) {
-        return new PerformanceCalculator(aPerformance, aPlay);
-    }
+class ComedyCalculator extends PerformanceCalculator {
 
-    function playFor(perf) {
-        return plays[perf.playID];
-    }
-
-    function totalVolumeCredits(data) {
-        return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-    }
-
-    function totalAmount(data) {
-        return data.performances.reduce((total, p) => total + p.amount, 0);
-    }
 }
 
 module.exports = createStatementData;
